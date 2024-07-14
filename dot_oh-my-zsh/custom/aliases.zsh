e51d11c423e35001af1fc3b81cf672b20f5d66f7
Randall Degges <r@rdegges.com>
2024-07-14 12:00:28 -0700

    Add .oh-my-zsh/custom/aliases.zsh

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,47 @@
+# Process handling.
+alias psg="ps aux | grep"
+alias ka9="killall -9"
+alias k9="kill -9"
+
+# Show human friendly numbers and colors.
+alias df="df -h"
+alias l="ls -ah"
+alias ll="ls -alh"
+alias du="du -sh"
+alias df="df -h"
+
+# Listing files.
+alias lsg="ll | grep"
+
+# Common shell functions.
+alias tf="tail -f"
+alias lh="ls -alt | head"
+alias tmux="TERM=xterm-256color tmux"
+alias cl="clear"
+alias ps="ps aux"
+
+# Quick file zipping.
+alias gz="tar -zcvf"
+
+# Vim helpers.
+alias v="nvim"
+
+# ZSH helpers.
+alias zr="source ~/.zshrc"
+
+# Python helpers.
+alias pt="python setup.py test"
+alias pyc="rm *.pyc"
+
+# Node helpers.
+alias nt="npm test"
+
+# Dotfile helpers.
+alias ca="chezmoi add"
+alias ce="chezmoi edit"
+alias ci="echo 'chezmoi init --apply git@github.com:<GITHUB_USERNAME>/dotfiles.git'"
+alias cu="chezmoi update --force"
+
+# Brew helpers.
+alias bd="brew bundle dump --force --file=~/.brew/Brewfile; chezmoi add ~/.brew/Brewfile"
+alias bi="brew install"
